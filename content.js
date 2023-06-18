afd57cc2394c1899016b86fb4aec4d82d679ed4c
tim-tom-88 <thomas.hodgson88@gmail.com>
2023-06-18 21:34:27 +0100

    update manifest

--- content.js
+++ content.js
@@ -1,24 +1,24 @@
-let lastSongTitle = ''
+let lastSongTitle = '';
 
 // Function to check for changes in media session metadata
 function checkMediaSessionMetadata() {
   if ('mediaSession' in navigator && 'metadata' in navigator.mediaSession) {
-    const songTitle = navigator.mediaSession.metadata.title
+    const songTitle = navigator.mediaSession.metadata.title;
     if (songTitle && songTitle !== lastSongTitle) {
-      lastSongTitle = songTitle
-      sendSongTitle(songTitle)
+      lastSongTitle = songTitle;
+      sendSongTitle(songTitle);
     }
   }
 }
 
 // Send the song title to the background script
 function sendSongTitle(songTitle) {
-  chrome.runtime.sendMessage({ songTitle })
+  chrome.runtime.sendMessage({ songTitle });
 }
 
 // Poll for changes in media session metadata every second
-setInterval(checkMediaSessionMetadata, 1000)
+setInterval(checkMediaSessionMetadata, 1000);
 
-// Mutation observer to detect changes in navigator.mediaSession.metadata object
-const observer = new MutationObserver(checkMediaSessionMetadata)
-observer.observe(navigator.mediaSession, { subtree: true, childList: true })
+// Mutation observer to detect changes in the document's head element
+const observer = new MutationObserver(checkMediaSessionMetadata);
+observer.observe(document.head, { subtree: true, childList: true });
